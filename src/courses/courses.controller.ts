57cdcef96356746794776fbb9a00133b1e959802
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-10-01 20:24:54 -0300

    Servico

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,15 @@
-import {Body, Controller, Get, HttpCode, HttpStatus, Param, Patch, Post, Res} from '@nestjs/common';
+import {Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res} from '@nestjs/common';
+import {CoursesService} from "./courses.service";
 //ENDPOINT DA REQUISITION
 // @ts-ignore
 // @ts-ignore
 @Controller('courses')
 export class CoursesController {
+
+    //O CONTAINER DE INJEÇÃO DE DEPENDENCIA JA FAZ ISSO PRA GENTE
+    //READONLY PQ NÃO MUDAMOS NADA DO SERVIÇO, NÃO FAZEMOS O "OVERRIDE AQUI"
+    constructor(private readonly coursesService:CoursesService) {
+    }
 
     @Get()
     findAll(@Res() response){
@@ -23,12 +29,17 @@ export class CoursesController {
         return body;
     }
 
+    //PATCH SO ATUALIZA UMA PARTE DO CONTEUDO ,PUT ATUALIZA TODO
     @Patch(':id')
     update(@Param('id') id:string,@Body() body)
     {
         return `Àtualizacao do Curso #${id}`;
     }
 
+    @Delete(':id')
+    remove(@Param() params){
+        return `Exclusao do Curso #${params.id}`;
+    }
 
 
 }
