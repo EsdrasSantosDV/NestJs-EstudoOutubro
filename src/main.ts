5502160e5a0d3b26f3bc34b9491104dfa5cb3f3d
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-10-01 21:55:00 -0300

    WHITE LIST FORBIDNONWHITELISTED TRANSFORM

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,18 @@ import {ValidationPipe} from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //VALIDACAO PRA REQUISICAO
-  app.useGlobalPipes(new ValidationPipe());
+  //WHITE LIST SO VAI PASSAR O QUE TIVER NO OBJETO VALIDATOR
+  //O forbid not white faz o seguinte se encontrar algo(um campo a mais por exemplo na requisicao post) Ele vai
+  // dar uma MENSAGEM DE ERRO,
+  //O TRANSFORM AUTOMATICAMENTE VAI TIPAR TANTO NO POST TANTO NO PATCH
+  //ELE JA SABE QUAL O TIPO VAI COLOCAR O QUE RECEBEU DA REQUISIÇÃO
+  app.useGlobalPipes(new ValidationPipe(
+      {
+        whitelist:true,
+        forbidNonWhitelisted:true,
+        transform:true
+      }
+  ));
   await app.listen(3000);
 }
 bootstrap();
