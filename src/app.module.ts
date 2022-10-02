1d33f112086ade5ef44aa0b574930f14fe3847af
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-10-02 07:03:44 -0300

    Configuração do Type Orm Pro mysql lembra disso CREATE USER 'esdras'@'%' IDENTIFIED WITH mysql_native_password BY 'Esdras@67'; GRANT ALL PRIVILEGES ON *.* TO 'esdras'@'%'; FLUSH PRIVILEGES;

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { CoursesModule } from './courses/courses.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+
 
 @Module({
-  imports: [CoursesModule],
+  imports: [CoursesModule,TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'esdras',
+    password: 'Esdras@67',
+    database: 'nest_typeorm_estudo',
+    autoLoadEntities:true,
+    synchronize:true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
